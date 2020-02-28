9c51a5c3205a1245195c94c179193a5505880cea
PetterV <petter.vilberg@gmail.com>
2020-02-28 22:45:12 +0100

    Initial commit

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GCData data = new GCData();
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameController");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadGameController(GCData loadedData){
+        data = loadedData;
+    }
+}
+
+public class GCData {
+
+}
---
+++ Assets/Scripts/Ship/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject movementTarget;
+    public int sailLevel = 0;
+    public int maxSailLevel = 2;
+    public float turnSpeed = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            IncreaseSailLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.S)){
+            DecreaseSailLevel();
+        }
+        MoveForwards();
+        if(Input.GetKey(KeyCode.A)){
+            TurnLeft();
+        }
+        if(Input.GetKey(KeyCode.D)){
+            TurnRight();
+        }
+    }
+
+    public void IncreaseSailLevel(){
+        if (sailLevel < maxSailLevel){
+            sailLevel++;
+        }
+    }
+
+    public void DecreaseSailLevel(){
+        if (sailLevel > 0){
+            sailLevel--;
+        }
+    }
+
+    public void MoveForwards(){
+        Vector3 heading = movementTarget.transform.position - transform.position;
+        transform.position += heading * sailLevel * Time.deltaTime;
+    }
+
+    public void TurnLeft(){
+        Vector3 newRotation = new Vector3(0 - GetCurrentTurnSpeed(), 0, 0);
+        transform.Rotate(newRotation);
+    }
+    public void TurnRight(){
+        Vector3 newRotation = new Vector3(GetCurrentTurnSpeed(), 0, 0);
+        transform.Rotate(newRotation);
+    }
+
+    float GetCurrentTurnSpeed(){
+        int sailLevelForTurnSpeed = maxSailLevel - sailLevel;
+        if (sailLevelForTurnSpeed < 1){
+            sailLevelForTurnSpeed = 1;
+        }
+        float currentTurnSpeed = turnSpeed * sailLevelForTurnSpeed;
+        return currentTurnSpeed;
+    }
+}
